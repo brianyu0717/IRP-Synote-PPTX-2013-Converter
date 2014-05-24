afcc691c912f7cd1d7ac502694b77305e8d2fe44
Brian Yu <by2g10@ecs.soton.ac.uk>
2014-05-24 20:34:51 +0100

    Commit

---
+++ SynotePPT2013Converter/Main.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace SynotePPT2013Converter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.button_BrowsePPTXfile = new System.Windows.Forms.Button();
+            this.textBox_PPTXPath = new System.Windows.Forms.TextBox();
+            this.button_Go = new System.Windows.Forms.Button();
+            this.textBox_Log = new System.Windows.Forms.TextBox();
+            this.label_Log = new System.Windows.Forms.Label();
+            this.backgroundWorker_Converter = new System.ComponentModel.BackgroundWorker();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button_BrowsePPTXfile
+            // 
+            this.button_BrowsePPTXfile.Location = new System.Drawing.Point(449, 28);
+            this.button_BrowsePPTXfile.Name = "button_BrowsePPTXfile";
+            this.button_BrowsePPTXfile.Size = new System.Drawing.Size(108, 23);
+            this.button_BrowsePPTXfile.TabIndex = 0;
+            this.button_BrowsePPTXfile.Text = "Browse PPTX File";
+            this.button_BrowsePPTXfile.UseVisualStyleBackColor = true;
+            this.button_BrowsePPTXfile.Click += new System.EventHandler(this.button_BrowsePPTXfile_Click);
+            // 
+            // textBox_PPTXPath
+            // 
+            this.textBox_PPTXPath.Location = new System.Drawing.Point(22, 28);
+            this.textBox_PPTXPath.Name = "textBox_PPTXPath";
+            this.textBox_PPTXPath.ReadOnly = true;
+            this.textBox_PPTXPath.Size = new System.Drawing.Size(420, 20);
+            this.textBox_PPTXPath.TabIndex = 1;
+            this.textBox_PPTXPath.Text = "None";
+            // 
+            // button_Go
+            // 
+            this.button_Go.Location = new System.Drawing.Point(449, 67);
+            this.button_Go.Name = "button_Go";
+            this.button_Go.Size = new System.Drawing.Size(108, 47);
+            this.button_Go.TabIndex = 4;
+            this.button_Go.Text = "Go";
+            this.button_Go.UseVisualStyleBackColor = true;
+            this.button_Go.Click += new System.EventHandler(this.button_Go_Click);
+            // 
+            // textBox_Log
+            // 
+            this.textBox_Log.Location = new System.Drawing.Point(22, 157);
+            this.textBox_Log.Multiline = true;
+            this.textBox_Log.Name = "textBox_Log";
+            this.textBox_Log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Log.Size = new System.Drawing.Size(535, 247);
+            this.textBox_Log.TabIndex = 5;
+            this.textBox_Log.TextChanged += new System.EventHandler(this.textBox_Log_TextChanged);
+            // 
+            // label_Log
+            // 
+            this.label_Log.AutoSize = true;
+            this.label_Log.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Log.Location = new System.Drawing.Point(19, 128);
+            this.label_Log.Name = "label_Log";
+            this.label_Log.Size = new System.Drawing.Size(31, 16);
+            this.label_Log.TabIndex = 6;
+            this.label_Log.Text = "Log";
+            // 
+            // backgroundWorker_Converter
+            // 
+            this.backgroundWorker_Converter.WorkerReportsProgress = true;
+            this.backgroundWorker_Converter.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_Converter_DoWork);
+            this.backgroundWorker_Converter.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_Converter_ProgressChanged);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(449, 128);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(108, 23);
+            this.progressBar.TabIndex = 7;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 416);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label_Log);
+            this.Controls.Add(this.textBox_Log);
+            this.Controls.Add(this.button_Go);
+            this.Controls.Add(this.textBox_PPTXPath);
+            this.Controls.Add(this.button_BrowsePPTXfile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Synote PPTX 2013 Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_BrowsePPTXfile;
+        private System.Windows.Forms.TextBox textBox_PPTXPath;
+        private System.Windows.Forms.Button button_Go;
+        private System.Windows.Forms.TextBox textBox_Log;
+        private System.Windows.Forms.Label label_Log;
+        private System.ComponentModel.BackgroundWorker backgroundWorker_Converter;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ SynotePPT2013Converter/Main.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using System.IO.Compression;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using NAudio;
+using NAudio.Wave;
+using NAudio.MediaFoundation;
+
+namespace SynotePPT2013Converter
+{
+    public partial class Main : Form
+    {
+        private string sourcePPTXPath = "";
+        private string intermediatePPTXPath = "";
+        private BackgroundWorker worker = new BackgroundWorker();
+
+        public Main()
+        {
+            InitializeComponent();
+            //button_Go_Click(null, null);
+        }
+
+        private void button_BrowsePPTXfile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PowerPoint 2013 files (.pptx)|*.pptx|All Files (*.*)|*.*";
+            openFileDialog.FilterIndex = 0;
+
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox_PPTXPath.Text = openFileDialog.FileName;
+            }
+        }
+
+
+
+        private static void WmaToWav(string wmaFile, string outputFile)
+        {
+            using (var reader = new MediaFoundationReader(wmaFile))
+            {
+                WaveFileWriter.CreateWaveFile(outputFile, reader);
+                reader.Close();
+            }            
+        }
+
+        private static void extractPPTXData(string pptxPath)
+        {
+            List<string> slideNotes = new List<string>();
+            List<string> slideTitles = new List<string>();
+            List<string> slideTexts = new List<string>();
+            List<object> slideNarrations = new List<object>();
+
+            PowerPoint.Application powerpointapplication = new PowerPoint.Application();
+            PowerPoint.Presentation powerpointpresentation
+                = powerpointapplication.Presentations.Open(pptxPath,
+                WithWindow: Microsoft.Office.Core.MsoTriState.msoFalse);
+            int slideCount = powerpointpresentation.Slides.Count;
+
+            foreach (PowerPoint.Slide sld in powerpointpresentation.Slides)
+            {
+                //Console.WriteLine(sld.Name);
+
+                #region get slide notes
+                if (sld.HasNotesPage == Microsoft.Office.Core.MsoTriState.msoTrue)
+                {
+                    slideNotes.Add(sld.NotesPage.Shapes[2].TextFrame.TextRange.Text);
+                }
+                #endregion
+
+                #region get slide Title and Text
+                string slideText = "";
+                string slideTitle = "";
+                foreach (PowerPoint.Shape shp in sld.Shapes)
+                {
+                    if (shp.HasTextFrame == Microsoft.Office.Core.MsoTriState.msoTrue)
+                    {
+                        if (slideTitle.Equals(string.Empty))
+                        {
+                            slideTitle = shp.TextFrame.TextRange.Text;
+                        }
+                        else
+                        {
+                            slideText += shp.TextFrame.TextRange.Text;
+                        }
+                    }
+                }
+                slideTitles.Add(slideTitle);
+                slideTexts.Add(slideText);
+                #endregion
+
+            }
+
+
+            powerpointpresentation.Close();
+            powerpointapplication.Quit();
+
+            //Console.WriteLine(slideCount);
+        }
+
+        private void button_Go_Click(object sender, EventArgs e)
+        {
+            string testPptxPath2010 = @"D:\Dropbox\ECS\Year4\AssistiveTech\Slides.pptx";
+
+            backgroundWorker_Converter.RunWorkerAsync();
+        }
+       
+        private void backgroundWorker_Converter_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string testPptxPath2013 = textBox_PPTXPath.Text; //@"D:\Dropbox\ECS\Year4\IRP\pptx13conv\pptx2013presentationTest.pptx";
+            string testPptxPath2013zip = Path.GetDirectoryName(testPptxPath2013)
+                + Path.DirectorySeparatorChar
+                + Path.GetFileNameWithoutExtension(testPptxPath2013)
+                + ".zip";
+
+            backgroundWorker_Converter.ReportProgress(0, "Processing " + testPptxPath2013);
+            Thread.Sleep(500);
+            //textBox_Log.Text += "\r\nProcessing " + testPptxPath2013;
+
+            // copy and rename pptx file to zip
+            File.Copy(testPptxPath2013, testPptxPath2013zip);
+            backgroundWorker_Converter.ReportProgress(10, "\r\nCopy and renamed " + testPptxPath2013 + " to " + testPptxPath2013zip);
+
+            // find paths of media*.wma files
+            string testPptxPath2013ZipExtracted = Path.GetDirectoryName(testPptxPath2013)
+                + Path.DirectorySeparatorChar
+                + Path.GetFileNameWithoutExtension(testPptxPath2013)
+                + Path.DirectorySeparatorChar;
+            ZipFile.ExtractToDirectory(testPptxPath2013zip, testPptxPath2013ZipExtracted);
+            string zipExtractedMediaFolderPath = testPptxPath2013ZipExtracted
+                + "ppt"
+                + Path.DirectorySeparatorChar
+                + "media"
+                + Path.DirectorySeparatorChar;
+            backgroundWorker_Converter.ReportProgress(20, "\r\nMedia Directory: " + zipExtractedMediaFolderPath);
+            Thread.Sleep(500);
+
+            List<string> narrationFies = new List<string>
+                (Directory.GetFiles(zipExtractedMediaFolderPath, "media*.wma"));
+
+            // run WmaToWav on the media*.wma files
+            foreach (string mediaPath in narrationFies)
+            {
+                backgroundWorker_Converter.ReportProgress(30, "\r\nConverting to wav: " + mediaPath);
+                Thread.Sleep(500);
+                string wavPath = Path.GetDirectoryName(mediaPath)
+                    + Path.DirectorySeparatorChar
+                    + Path.GetFileNameWithoutExtension(mediaPath)
+                    + ".wav";
+                WmaToWav(mediaPath, wavPath);
+            }
+            Thread.Sleep(500);
+            backgroundWorker_Converter.ReportProgress(40, "\r\nAll media converted");
+
+            // zip up the intermediate folder
+            backgroundWorker_Converter.ReportProgress(50, "\r\nDeleting " + testPptxPath2013zip);
+            File.Delete(testPptxPath2013zip);
+            //DeleteDirectory(testPptxPath2013ZipExtracted);
+
+
+            string pptxIntermediatePath =
+                Path.GetDirectoryName(testPptxPath2013)
+                + Path.DirectorySeparatorChar
+                + Path.GetFileNameWithoutExtension(testPptxPath2013)
+                + "_INTERMEDIATE.pptx";
+
+            if (File.Exists(pptxIntermediatePath))
+            {
+                File.Delete(pptxIntermediatePath);
+            }
+
+            Thread.Sleep(500);
+            backgroundWorker_Converter.ReportProgress(60, "\r\nZipping " + testPptxPath2013ZipExtracted);
+            ZipFile.CreateFromDirectory(testPptxPath2013ZipExtracted,
+                pptxIntermediatePath);
+
+            Thread.Sleep(500);
+            
+            Process.Start("cmd.exe", "/c " + @"rmdir /s/q " + testPptxPath2013ZipExtracted);
+            //Directory.Delete(testPptxPath2013ZipExtracted, true);
+
+            //MessageBox.Show("Conversion Completed. Please now run the pptx converter on\r\n"
+            //    + pptxIntermediatePath);
+
+            Thread.Sleep(500);
+            backgroundWorker_Converter.ReportProgress(100, "\r\nCompleted");
+        }
+
+        private void backgroundWorker_Converter_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            
+            textBox_Log.Text += e.UserState.ToString();
+            progressBar.Value = e.ProgressPercentage;
+
+        }
+
+        private void textBox_Log_TextChanged(object sender, EventArgs e)
+        {
+            // scroll to the end of the text box
+            textBox_Log.SelectionStart = textBox_Log.Text.Length;
+            textBox_Log.ScrollToCaret();
+        }
+
+    }
+}
